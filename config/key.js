6f317ca3abd86fd3186fe4f5d348afbefc385aa5
nitesh-phulse <niteshphulse4@gmail.com>
2023-11-05 16:39:18 +0530

    node

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI:"mongodb+srv://niteshphulse4:nitesh7090@cluster0.z7dd9ys.mongodb.net/myFirstDatabase"
+    // "mongodb+srv://kumar:awxhnpt6Mf9keSRI@cluster0.zsmd20g.mongodb.net/?retryWrites=true&w=majority"
+}
